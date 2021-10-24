49ea7c6e3e3f6bda3f8a3ce6a38d30b169ed27a1
tjokroo <gartinzchannel@gmail.com>
2021-10-24 22:32:59 +0700

    menambahkan projek baru

---
+++ Latihan1.py
@@ -0,0 +1,10 @@
+Nama=input("Siapa nama anda:")
+namap=input("biasa dipanggil siapa:")
+nim=input("berapa nomor nim anda:")
+ttl=input("dimana dan kapan anda lahir:")
+umur=input("berapakah umur anda:")
+alamat=input("dimanakah anda tinggal:")
+no=input("berapakah nomor telpon anda:")
+print("Jadi nama anda adalah",Nama,"biasanya anda dipanggil",namap,
+"nomor nim adalah",nim,"anda lahir di",ttl,"sekarang anda berumur",umur,
+"dan beralamat di",alamat,"dengan nomor telpon",no)
